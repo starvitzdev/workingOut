fef20ff1d3045769cce8fd67bfd925f7c4ce110f
starvitzdev <starvitz@pm.me>
2025-01-01 23:45:39 +0100

    My first commit on my great journey

---
+++ main.py
@@ -0,0 +1,9 @@
+print("This is a test to write some python code.")
+
+
+def main():
+    print("This is inside a main function")
+
+print("I am not saying this is the best code to be written.")
+print("But it is the first step on a great journey.")
+print("Even do this is not very impressive ;)")
