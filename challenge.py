4ec35947dfdeb1baee077b28055b0d3ec20720ba
starvitzdev <starvitz@pm.me>
2025-01-02 22:17:56 +0100

    Honestly pathetic performance by me today. But with only 3 hours of sleep a module finished and a grandma who passed away, this isn't half bad

---
+++ challenge.py
@@ -0,0 +1,10 @@
+# My grandma passed today. And honestly i almost forgot about the streak.
+
+# Challenge 1: Print Numbers from 1 to 10
+for i in range(10):
+    print(i)
+
+# Challenge 2: Print Odd Numbers Less Than 100
+for i in range(100):
+    if (i % 2 != 0):
+        print(i)
